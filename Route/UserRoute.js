fb66283f252be4579753c8349ef3351eae41f10c
badshah-coder <badshah11ah@gmail.com>
2025-07-18 11:25:52 +0500

    Update UserRoute.js

--- Route/UserRoute.js
+++ Route/UserRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, addUser, getAllUsers } from "../Controller/UserController.js";
+import { loginUser, addUser, getAllUsers, deleteUser } from "../Controller/UserController.js";
 
 const router = express.Router();
 
@@ -11,5 +11,8 @@ router.post("/add", addUser);
 
 // Admin: Get all users
 router.get("/all", getAllUsers);
+
+// Admin: Delete user
+router.delete("/delete/:id", deleteUser);
 
 export default router;
